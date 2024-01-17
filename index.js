11b278511c2c1fef1e09e37b4333617a63825b05
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-17 23:35:27 +0530

    fix: updated index.js with routes to send alerts and receive confirmational message

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ const sockerService = new SocketService(httpServer);
 sockerService.initListeners();
 
 app.use('/api', userDataRoute);
+app.get('/api/', (req, res) => {
+    sockerService.getIO().emit('alertMsg', 'Hello from server');
+    res.status(200).send('Hello World');
+})
+
+app.post('/api/submit', (req, res) => {
+    console.log(req.body)
+})
 
 httpServer.listen(4000, () => {
     console.log('Server is running on port 4000');
