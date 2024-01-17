b6fe96389a7149cf92404d563f90a2cf24fad0cb
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-17 22:17:48 +0530

    fix: send user data upon connection

--- services/socket.js
+++ services/socket.js
@@ -1,4 +1,13 @@
 const { Server } = require('socket.io');
+const offuser = {
+    "lab_no": "L001",
+    "system_id": "C001",
+    "user": "student",
+    "id_no": "56700786",
+    "dob": "12345678",
+    "exam_name": "ONLINE EXAM"
+}
+
 
 class SocketService {
     constructor(server) {
@@ -22,6 +31,8 @@ class SocketService {
             });
             socket.on("system_online", (data) => {
                 console.log("System online: ",data);
+                io.to(data.socket_id).emit("userData", offuser);
+                console.log("Sent data: ",data.socket_id);
             });
         });
     }
