1f8f4100f9cdbf2573428358ceeaeb6d1dd785f7
Bikram Ghuku <bikramghuku05@gmail.com>
2024-01-22 23:33:46 +0530

    fix: added get system by socket id

--- services/db.js
+++ services/db.js
@@ -6,12 +6,20 @@ class DbService {
     }
 
     async addUpdateSystem(data) {
-        const { sys_no, lab_no, socket_id } = data
+        const { sys_no, lab_no, socket_id, status } = data
         await this.prisma.system.upsert({
             where: { labNo_sysNo: { labNo: lab_no, sysNo: sys_no } },
-            update: { socketId: socket_id },
-            create: { sysNo: sys_no, labNo: lab_no, socketId: socket_id, status: "online" },
+            update: { socketId: socket_id, status: status },
+            create: { sysNo: sys_no, labNo: lab_no, socketId: socket_id, status: status },
         })
+    }
+
+    async getSystemBySocketId(socket_id) {
+        console.log("Socket id: ",socket_id);
+        const system = await this.prisma.system.findMany({
+            where: { socketId: socket_id }
+        })
+        return system[0];
     }
 }  
 
